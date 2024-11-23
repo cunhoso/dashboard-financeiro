152ae43479ad1a5f2a0413e880a59d855b76b7b7
Dan Santos <110039343+EngDanSantos@users.noreply.github.com>
2024-11-23 16:12:14 -0300

    crud

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from core.models import Funcionario, Cliente, Gasto, Lucro
 
-# Register your models here.
+class DbDisplay(admin.ModelAdmin):
+    list_display = ("nome",)
+    list_display_links = ("nome",)
+    search_fields = ("nome",)
+
+
+admin.site.register(Funcionario, DbDisplay)
+admin.site.register(Lucro)
+admin.site.register(Gasto)
+admin.site.register(Cliente)
+
+
